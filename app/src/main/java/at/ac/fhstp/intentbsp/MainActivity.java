22786a79b5a284d2b1952d3537facc6fe0358f67
Master-Chaos <thomas_pointner@gmx.at>
2018-09-23 20:52:46 +0200

    make comments

--- app/src/main/java/at/ac/fhstp/intentbsp/MainActivity.java
+++ app/src/main/java/at/ac/fhstp/intentbsp/MainActivity.java
@@ -10,8 +10,8 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private final static int REQUEST_SUB_ACT = 7;
-    private final static String TAG = "MainActivity";
+    private final static int REQUEST_SUB_ACT = 7; //DEFINE A REQUEST_SUB_ACT NUMBER
+    private final static String TAG = "MainActivity"; //DEFINE A TAG WITH A STING MAINACTIVITY
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,49 +19,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void btnOnClick(View view)
+    public void btnOnClick(View view) //FUNCTION FOR OPEN ACTIVITY BUTTON
     {
-        //..
-        Intent intent = new Intent(this, SubActivity.class); //ERSTELLT DIE KLASSEN
-        intent.putExtra("count", 7);
-        intent.putExtra("name", "Max Maier");
-        //startActivity(intent);
-        startActivityForResult(intent, REQUEST_SUB_ACT);
+        Intent intent = new Intent(this, SubActivity.class); //CREATE A NEW INTENT CLASS FOR SWITCH TO SUBACTIVITY VIEW
+        intent.putExtra("count", 7); //ADD EXTRA INPUTDATA
+        intent.putExtra("name", "Max Maier"); //ADD EXTRA INPUTDATA
+        //startActivity(intent); //START ACTIVITY OF THE CREATED INTENT
+        startActivityForResult(intent, REQUEST_SUB_ACT); //START ACTIVITY FOR RESULT
     }
 
-    public void btnOpenUrl(View view) //URL ÖFFNEN Foliensatz 2 Folie 16
+    public void btnOpenUrl(View view) //FUNCTION FOR OPEN URL BUTTON (FOILSET 2; FOIL 16)
     {
-        Log.i(TAG, "TODO open url ...");
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.at"));
-        startActivity(intent);
+        Log.i(TAG, "TODO open url ..."); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.at")); //CREATE A NEW INTENT CLASS FOR OPEN A URL
+        startActivity(intent); //START ACTIVITY OF THE CREATED INTENT
     }
 
-    public void btnOpenMedia(View view) //Media ÖFFNEN Foliensatz 2 Folie 16
+    public void btnOpenMedia(View view) //FUNCTION FOR OPEN MEDIA BUTTON (FOILSET 2; FOIL 16)
     {
-        Log.i(TAG, "TODO open Media ...");
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(intent);
+        Log.i(TAG, "TODO open Media ..."); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //CREATE A NEW INTENT CLASS FOR MEDIA
+        startActivity(intent); //START ACTIVITY OF THE CREATED INTENT
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_SUB_ACT)
+        if (requestCode == REQUEST_SUB_ACT) //CHECK IF REQUESTCODE IS REQUEST_SUB_ACT
         {
-            Log.i(TAG, "result code" + resultCode);
-            if (resultCode == RESULT_OK)
+            Log.i(TAG, "result code" + resultCode); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
+            if (resultCode == RESULT_OK) //CHECK IF RESULTCODE IS RESULT_OK
             {
-                Log.i(TAG, "result ok!");
-                Log.i(TAG, "score " + data.getFloatExtra("score", 0.0f));
-                if (data.hasExtra("address")){
-                    Log.i(TAG, "address " + data.getStringExtra("address"));
+                Log.i(TAG, "result ok!"); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
+                Log.i(TAG, "score " + data.getFloatExtra("score", 0.0f)); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
+                if (data.hasExtra("address")){ //CHECK IF INTENT I HAS EXTRA DATA BY ADDRESS
+                    Log.i(TAG, "address " + data.getStringExtra("address")); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE:i = INFO
                 }
             }
             else
             {
-                Log.w(TAG, "result code not ok (Canceld)"); //w = warning
+                Log.w(TAG, "result code not ok (Canceld)"); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: w = WARNING
             }
         }
     }
--- app/src/main/java/at/ac/fhstp/intentbsp/SubActivity.java
+++ app/src/main/java/at/ac/fhstp/intentbsp/SubActivity.java
@@ -6,30 +6,30 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class SubActivity extends AppCompatActivity {
-    private static final String TAG = "SubActi";
+    private static final String TAG = "SubActi";  //DEFINE A TAG WITH A STING SUBACTI
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
-        Intent i = getIntent();
-        Log.i(TAG, "intent data:");
-        if (i.hasExtra("count")){
-            int cnt = i.getIntExtra("count", 0); //i für info I ist loglevel d für debug, e für error
-            Log.e(TAG, "error with error count " + cnt + "!!!");
+        Intent i = getIntent(); //GET THE INTENT FROM MAIN AN SAVE IT IN A NEW INTENT VAR I
+        Log.i(TAG, "intent data:"); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
+        if (i.hasExtra("count")){ //CHECK IF INTENT I HAS EXTRA DATA BY COUNT
+            int cnt = i.getIntExtra("count", 0); //CREATE INT CNT TO SAVE VALUE OF COUNT i für info I ist loglevel d für debug, e für error
+            Log.e(TAG, "error with error count " + cnt + "!!!"); //OUTPUT MESSAGE AND VALUE OF CNT TO LOGCAT FOR LOG IT - LOGLEVELTYPE: e = ERROR
         }
         else {
-            Log.i(TAG, "no count defined");
+            Log.i(TAG, "no count defined"); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
         }
 
-        if (i.hasExtra("name")){
-            Log.i(TAG, "name:" + i.getStringExtra("name")); //OUTPUT IN CATLOG
+        if (i.hasExtra("name")){ //CHECK IF INTENT I HAS EXTRA DATA BY NAME
+            Log.i(TAG, "name:" + i.getStringExtra("name")); //OUTPUT MESSAGE TO LOGCAT FOR LOG IT - LOGLEVELTYPE: i = INFO
         }
 
-        i.putExtra("score", 0.77f); //Wert soll zwischen 0 oder 1 sein fügt die werte hinzu zum mainwerte
-        i.putExtra("address", "1120 Vienna"); //fügt die werte hinzu zum mainwerte
-        setResult(RESULT_OK, i);
+        i.putExtra("score", 0.77f); //ADD VALUE FOR SCORE TO I (VALUE MUST BE BETWEEN 0 AND 1)
+        i.putExtra("address", "1120 Vienna"); //ADD VALUE FOR ADDRESS TO I
+        setResult(RESULT_OK, i); //SET RESULT TO RESULTVALUE RESULT_OK
 
     }
 }
